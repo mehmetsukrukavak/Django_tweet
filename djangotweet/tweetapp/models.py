b0ec499a1992f48658e19ba79e4292cc3673b25f
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-18 10:19:32 +0300

    list page and add page

--- djangotweet/tweetapp/models.py
+++ djangotweet/tweetapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    nickname = models.CharField(max_length=50)
+    message = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"nickname: {self.nickname} message : {self.message}"
--- djangotweet/tweetapp/views.py
+++ djangotweet/tweetapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def list_tweet(request):
-    return render(request,'tweetapp/listtweet.html')
+    all_tweets = models.Tweet.objects.all()
+    tweet_dict = { "tweets": all_tweets}
+    return render(request,'tweetapp/listtweet.html', context=tweet_dict)
 
 
 def add_tweet(request):

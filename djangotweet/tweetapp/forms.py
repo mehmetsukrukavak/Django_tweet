4643d006a63f461cf660b13a48dbd090903661cf
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-18 16:27:07 +0300

    django form customize

--- djangotweet/tweetapp/forms.py
+++ djangotweet/tweetapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class AddTweetForm(forms.Form):
     nickname_input = forms.CharField(label="Nickname", max_length=50)
-    message_input = forms.CharField(label="Message", max_length=100)
+    message_input = forms.CharField(label="Message", max_length=100, widget=forms.Textarea(attrs={"class":"tweetmessage"}))

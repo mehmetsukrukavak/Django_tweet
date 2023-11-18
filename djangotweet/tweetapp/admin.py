bdb2b193162e0d1b2e5cbffa822770e0940638e6
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-18 11:15:44 +0300

    admin operations

--- djangotweet/tweetapp/admin.py
+++ djangotweet/tweetapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+#from . import models
+from tweetapp.models import Tweet
 
 # Register your models here.
+class TweetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Nickname Group",{"fields":["nickname"]}),
+        ("Message Group",{"fields":["message"]})
+    ]
+   #fields =['message','nickname']
+
+
+admin.site.register(Tweet, TweetAdmin)
